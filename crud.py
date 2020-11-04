655066fb3c097fdf0f1d961176ee2e77f398cca4
Kelsi-Wolter <kelsi.wolter@gmail.com>
2020-11-04 14:51:09 +0000

    Completed session commit code for create functions in crud.py

--- crud.py
+++ crud.py
@@ -12,9 +12,38 @@ if __name__ == '__main__':
 
 def create_user(first_name, last_name, email, password):
 
+    user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
 def create_roaster(name, address, phone_number, hours, image, website, coffee_link, shipping_link, avg_rating):
 
-def create_list(list_type, list_name, user)
+    roaster = Roaster(name=name, address=address, phone_number=phone_number, hours=hours,
+    image=image, website=website, coffee_link=coffee_link, shipping_link=shipping_link,
+    avg_rating=avg_rating)
+
+    db.session.add(roaster)
+    db.session.commit()
+
+    return roaster
+
+def create_list(list_type, list_name, user):
+
+    new_list = List(list_type=list_type, list_name=list_name, user=user)
+
+    db.session.add(new_list)
+    db.session.commit()
+
+    return new_list
 
 def create_entry(entry_list, roaster, score, note):
 
+    entry = Entry(entry_list=entry_list, roaster=roaster, score=score, note=note)
+
+    db.session.add(entry)
+    db.session.commit()
+
+    return entry
